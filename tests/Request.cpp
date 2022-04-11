cc1ce9c9cfaff09140085477a4a277f44f2965a4
Ian Clester <ijc@ijc8.me>
2022-04-11 15:21:36 -0400

    Parse results: ad-hoc, JSON.

--- tests/Request.cpp
+++ tests/Request.cpp
@@ -2,20 +2,30 @@
 #include <juce_core/juce_core.h>
 #include "catch.hpp"
 
-TEST_CASE("make an HTTP request", "[board]") {
+TEST_CASE("fetch a game from lichess", "[fetch]") {
     juce::StringPairArray responseHeaders;
     int statusCode = 0;
 
     auto options = juce::URL::InputStreamOptions(juce::URL::ParameterHandling::inAddress)
-        .withConnectionTimeoutMs(10000)
+        .withConnectionTimeoutMs(-1)
         .withResponseHeaders(&responseHeaders)
         .withStatusCode(&statusCode);
 
 
-    auto stream = juce::URL("https://lichess.org/api/stream/game/1lkY0VZk").createInputStream(options);
+    auto stream = juce::URL("https://lichess.org/api/stream/game/nbdMSf7h").createInputStream(options);
     REQUIRE(stream != nullptr);
     REQUIRE(statusCode == 200);
     std::cout << "Headers: " << responseHeaders.getDescription() << std::endl;
     std::cout << "Response body:" << std::endl;
-    std::cout << stream->readEntireStreamAsString();
+    juce::String line;
+    // juce::String prefix = "\"fen\":\"";
+    do {
+        line = stream->readNextLine();
+        // auto segment = line.fromFirstOccurrenceOf(prefix, false, false).upToFirstOccurrenceOf("\"", false, false);
+        juce::var v = juce::JSON::parse(line);
+        if (v.hasProperty("id")) {
+            std::cout << "Final FEN: " << std::endl;
+        }
+        std::cout << v.getProperty("fen", "").toString() << std::endl;
+    } while (!line.isEmpty());
 }
