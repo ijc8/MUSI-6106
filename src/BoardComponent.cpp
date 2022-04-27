cdcaab742f7e732d4146dbb4b4457b6a18fa6057
Ian Clester <ijc@ijc8.me>
2022-04-26 23:30:50 -0400

    Draw pieces, remove inner `Piece` class.

--- src/BoardComponent.cpp
+++ src/BoardComponent.cpp
@@ -38,26 +38,22 @@ void BoardComponent::paint(juce::Graphics &g) {
     static const juce::Colour lightSquare(240, 217, 181);
     static const juce::Colour darkSquare(181, 136, 99);
 
-    // NOTE: We assume our container is maintaining our 1:1 aspect ratio.
+    // Draw squares.
+    // NOTE: We assume our parent is maintaining our 1:1 aspect ratio.
     int size = getWidth();
     float squareSize = (float)size / BoardSize;
     g.fillCheckerBoard(juce::Rectangle<float>(0, 0, size, size), squareSize, squareSize, lightSquare, darkSquare);
-}
 
-void BoardComponent::resized() {
-    auto area = getBounds();
-    int squareHeight = area.getHeight() / BoardSize;
-    int squareWidth = area.getWidth() / BoardSize;
-
-    int ypos = area.getHeight();
-    for (int row = 0; row < BoardSize; row++) {
-        ypos -= squareHeight;
-        int xpos = 0;
-        for (int col = 0; col < BoardSize; col++) {
-            // m_AllSquares[row][col]->setBounds(xpos, ypos, squareWidth,
-            //                                   squareHeight);
-            xpos += squareWidth;
-        }
+    // Draw pieces.
+    float pieceHeight = squareSize * .8;
+    float topMargin = (squareSize - pieceHeight) / 2.0;
+    for (auto [square, piece] : AppState::getInstance().getGame().getPieceMap()) {
+        float x = squareSize * square.file;
+        float y = squareSize * (7 - square.rank);
+        juce::Image &image = pieceImages[piece];
+        float pieceWidth = pieceHeight * image.getWidth() / image.getHeight();
+        float leftMargin = (squareSize - pieceWidth) / 2.0;
+        g.drawImage(image, juce::Rectangle<float>(x + leftMargin, y + topMargin, pieceWidth, pieceHeight));
     }
 }
 
@@ -115,7 +111,7 @@ void BoardComponent::buttonClicked(juce::Button *button) {
 }
 
 void BoardComponent::changeListenerCallback(juce::ChangeBroadcaster *source) {
-    std::list<Piece *> pieceList;
+    // std::list<Piece *> pieceList;
     // for (Piece &piece : m_AllPieces) {
     //     piece.setVisible(false);
     //     pieceList.push_front(&piece);
@@ -123,27 +119,27 @@ void BoardComponent::changeListenerCallback(juce::ChangeBroadcaster *source) {
 
     auto pieceMap = AppState::getInstance().getGame().getPieceMap();
     for (const auto [square, piece] : pieceMap) {
-        for (Piece *guiPiece : pieceList) {
-            if (guiPiece->getId().toStdString() ==
-                std::string{piece.toChar()}) {
-                guiPiece->setVisible(true);
-                // guiPiece->placeAt(findSquare(square.toString()));
-                pieceList.remove(guiPiece);
-                break;
-            }
-        }
+        // for (Piece *guiPiece : pieceList) {
+        //     if (guiPiece->getId().toStdString() ==
+        //         std::string{piece.toChar()}) {
+        //         guiPiece->setVisible(true);
+        //         // guiPiece->placeAt(findSquare(square.toString()));
+        //         pieceList.remove(guiPiece);
+        //         break;
+        //     }
+        // }
     }
     onStateChange(State::Idle);
 }
 
 void BoardComponent::actionListenerCallback(const juce::String &message) {
     if (message.contains("Preview")) {
-        if (m_SelectedPiece) {
-            juce::String newMessage = message.substring(0, 8) +
-                                      m_SelectedPiece->getSquareId() +
-                                      message.substring(8, 10);
-            sendActionMessage(newMessage);
-        }
+        // if (m_SelectedPiece) {
+        //     juce::String newMessage = message.substring(0, 8) +
+        //                               m_SelectedPiece->getSquareId() +
+        //                               message.substring(8, 10);
+        //     sendActionMessage(newMessage);
+        // }
     }
 }
 
@@ -158,19 +154,19 @@ void BoardComponent::onModeChange(BoardComponent::Mode newMode) {
     m_CurrentMode = newMode;
 }
 
-void BoardComponent::selectPiece(Piece &piece) {
-    if (m_SelectedPiece) {
-        m_SelectedPiece->setSelected(false);
-        if (m_SelectedPiece->getId() != piece.getId())
-            sendActionMessage("Deselect " + m_SelectedPiece->getId());
-    }
-    m_SelectedPiece = &piece;
-    m_SelectedPiece->setSelected(true);
-    m_SelectedPiece->setToggleState(true, juce::dontSendNotification);
-    highlightPossibleMoves(m_SelectedPiece);
-    sendActionMessage("Select " + m_SelectedPiece->getId());
-    onStateChange(State::Placing);
-}
+// void BoardComponent::selectPiece(Piece &piece) {
+//     if (m_SelectedPiece) {
+//         m_SelectedPiece->setSelected(false);
+//         if (m_SelectedPiece->getId() != piece.getId())
+//             sendActionMessage("Deselect " + m_SelectedPiece->getId());
+//     }
+//     m_SelectedPiece = &piece;
+//     m_SelectedPiece->setSelected(true);
+//     m_SelectedPiece->setToggleState(true, juce::dontSendNotification);
+//     highlightPossibleMoves(m_SelectedPiece);
+//     sendActionMessage("Select " + m_SelectedPiece->getId());
+//     onStateChange(State::Placing);
+// }
 
 // BoardComponent::Square *
 // BoardComponent::findSquare(const std::string &squareId) const {
@@ -187,17 +183,17 @@ void BoardComponent::selectPiece(Piece &piece) {
 //     return findSquare(piece.getSquareId());
 // }
 
-void BoardComponent::highlightPossibleMoves(const Piece *piece) {
-    if (piece) {
-        Chess::Game game = AppState::getInstance().getGame();
-        std::unordered_set moves =
-            game.generateMoves(Chess::Square(piece->getSquareId()));
-        for (const Chess::Move &move : moves) {
-            // Square *square = findSquare(move.dst.toString());
-            // square->isCandidate(true);
-        }
-    }
-}
+// void BoardComponent::highlightPossibleMoves(const Piece *piece) {
+//     if (piece) {
+//         Chess::Game game = AppState::getInstance().getGame();
+//         std::unordered_set moves =
+//             game.generateMoves(Chess::Square(piece->getSquareId()));
+//         for (const Chess::Move &move : moves) {
+//             Square *square = findSquare(move.dst.toString());
+//             square->isCandidate(true);
+//         }
+//     }
+// }
 
 void BoardComponent::resetPossibleMoves() {
     for (int row = 0; row < BoardSize; row++) {
@@ -211,12 +207,12 @@ void BoardComponent::resetPossibleMoves() {
 void BoardComponent::onStateChange(BoardComponent::State newState) {
     switch (newState) {
     case State::Idle:
-        if (m_SelectedPiece) {
-            m_SelectedPiece->setToggleState(false, juce::dontSendNotification);
-            m_SelectedPiece->setSelected(false);
-            sendActionMessage("Deselect " + m_SelectedPiece->getId());
-        }
-        m_SelectedPiece = nullptr;
+        // if (m_SelectedPiece) {
+        //     m_SelectedPiece->setToggleState(false, juce::dontSendNotification);
+        //     m_SelectedPiece->setSelected(false);
+        //     sendActionMessage("Deselect " + m_SelectedPiece->getId());
+        // }
+        // m_SelectedPiece = nullptr;
         resetPossibleMoves();
         break;
     case State::Switching:
--- src/BoardComponent.h
+++ src/BoardComponent.h
@@ -22,97 +22,37 @@ class BoardComponent: public juce::Component,
         PGN,
     };
 
-    class Piece: public juce::ImageButton {
-      public:
-        Piece(const char *resourceName, uint8_t name,
-              std::string intialSquareId)
-            : m_Name(name), m_SquareId(intialSquareId),
-              m_Team(
-                  (isupper(name) ? Chess::Color::White : Chess::Color::Black)) {
-            int size;
-            const char *data =
-                ChessImageData::getNamedResource(resourceName, size);
-            m_Image = juce::ImageFileFormat::loadFrom(data, size);
-            setImages(
-                false, true, true, m_Image, 1, juce::Colours::transparentBlack,
-                juce::Image(nullptr), 0.5, juce::Colours::transparentWhite,
-                juce::Image(nullptr), 0.5, juce::Colours::transparentWhite);
-            setSize(80, 80);
-        };
-        ~Piece() = default;
+    // void mouseDrag(const juce::MouseEvent &event) override {
+    //     if (!mIsSelected) {
+    //         triggerClick();
+    //     }
+    //     juce::MouseEvent relEvent =
+    //         event.getEventRelativeTo(getParentComponent());
+    //     setCentrePosition(relEvent.getPosition());
+    //     mWasBeingDragged = true;
+    // }
 
-        juce::String getId() const {
-            return juce::String::charToString(m_Name);
-        };
-        void setSquareId(std::string newId) { m_SquareId = newId; };
-        std::string getSquareId() const { return m_SquareId; };
-        Chess::Color getTeam() const { return m_Team; };
-        bool operator==(const Piece &lhs) const {
-            return (this->m_Name == lhs.m_Name &&
-                    this->m_SquareId == lhs.m_SquareId);
-        }
-
-        bool isAlly(const Piece &piece) const { return m_Team == piece.m_Team; }
-
-        // void placeAt(const Square *square) {
-        //     m_Square = square;
-        //     m_SquareId = square->getId();
-        //     setBounds(square->getBounds());
-        // }
-
-        void resized() override {
-            // if (m_Square)
-            //     setBounds(m_Square->getBounds());
-        }
-
-        void setSelected(bool isSelected) { mIsSelected = isSelected; }
-
-        void mouseDown(const juce::MouseEvent &event) override {
-            ImageButton::mouseDown(event);
-        }
-
-        void mouseDrag(const juce::MouseEvent &event) override {
-            if (!mIsSelected) {
-                triggerClick();
-            }
-            juce::MouseEvent relEvent =
-                event.getEventRelativeTo(getParentComponent());
-            setCentrePosition(relEvent.getPosition());
-            mWasBeingDragged = true;
-        }
-
-        void mouseUp(const juce::MouseEvent &event) override {
-            if (mWasBeingDragged) {
-                for (juce::Component *component :
-                     getParentComponent()->getChildren()) {
-                    if (component != this &&
-                        component->getBoundsInParent().contains(
-                            event.getEventRelativeTo(getParentComponent())
-                                .getPosition())) {
-                        component->mouseDown(event);
-                        component->mouseUp(event);
-                    }
-                }
-                mWasBeingDragged = false;
-            } else {
-                ImageButton::mouseUp(event);
-            }
-            resized();
-        }
-
-      private:
-        bool mIsSelected = false;
-        bool mWasBeingDragged = false;
-        // const Square *m_Square = nullptr;
-        juce::Image m_Image;
-        uint8_t m_Name;
-        std::string m_SquareId;
-        const Chess::Color m_Team = Chess::Color::White;
-    };
+    // void mouseUp(const juce::MouseEvent &event) override {
+    //     if (mWasBeingDragged) {
+    //         for (juce::Component *component :
+    //                 getParentComponent()->getChildren()) {
+    //             if (component != this &&
+    //                 component->getBoundsInParent().contains(
+    //                     event.getEventRelativeTo(getParentComponent())
+    //                         .getPosition())) {
+    //                 component->mouseDown(event);
+    //                 component->mouseUp(event);
+    //             }
+    //         }
+    //         mWasBeingDragged = false;
+    //     } else {
+    //         ImageButton::mouseUp(event);
+    //     }
+    //     resized();
+    // }
 
     BoardComponent();
     void paint(juce::Graphics &g) override;
-    void resized() override;
     void buttonClicked(juce::Button *button) override;
     void changeListenerCallback(juce::ChangeBroadcaster *source) override;
     void actionListenerCallback(const juce::String &message) override;
@@ -124,15 +64,14 @@ class BoardComponent: public juce::Component,
 
     Mode m_CurrentMode = Mode::PVP;
     State m_CurrentState = State::Idle;
-    Piece *m_SelectedPiece = nullptr;
-    // Square *m_AllSquares[BoardSize][BoardSize]{nullptr};
+    // Piece *m_SelectedPiece = nullptr;
 
     std::unordered_map<Chess::Piece, juce::Image> pieceImages;
 
-    void selectPiece(Piece &piece);
+    // void selectPiece(Piece &piece);
     // Square *findSquare(const std::string &squareId) const;
     // Square *findSquare(const Piece &piece) const;
-    void highlightPossibleMoves(const Piece *piece);
+    // void highlightPossibleMoves(const Piece *piece);
     void resetPossibleMoves();
     void onStateChange(State newState);
 };
